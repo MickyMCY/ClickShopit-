ffdbaa5ac8cb4a7804275f2854fd799137aabbed
MickyMCY <mickybrow22donodoscanais@gmail.com>
2025-06-14 15:08:44 +0200

    Update produto.js

--- produto.js
+++ produto.js
@@ -1,20 +1,21 @@
 const params = new URLSearchParams(location.search);
 const id = parseInt(params.get("id"));
-const produtos = JSON.parse(localStorage.getItem("produtos"));
-const produto = produtos.find(p => p.id === id);
+const prod = allProducts.find(p => p.id === id);
 const div = document.getElementById("product-detail");
-
 div.innerHTML = `
-  <h2>${produto.nome}</h2>
-  <img src="${produto.img}" alt="${produto.nome}" style="width:100%; max-width:300px;" />
-  <p>${produto.desc}</p>
-  <button onclick="addToCart(${produto.id})">Adicionar ao carrinho</button>
-  <button onclick="location.href='checkout.html'">Comprar agora</button>
-`;
-
-function addToCart(id) {
-  const carrinho = JSON.parse(localStorage.getItem("carrinho") || "[]");
-  carrinho.push(id);
-  localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  <div class="product-detail">
+    <img src="${prod.img}" alt="${prod.nome}" />
+    <h2>${prod.nome}</h2>
+    <div class="price">${prod.price} MT</div>
+    ${prod.soldOut ? '<div class="sold-out">Esgotado</div>' : ''}
+    <p>Descrição breve do produto vai aqui. Pode personalizar.</p>
+    <button onclick="addCart(${prod.id})" ${prod.soldOut ? 'disabled' : ''}>Adicionar ao carrinho</button>
+    <button onclick="goCheckout()" ${prod.soldOut ? 'disabled' : ''}>Comprar agora</button>
+  </div>`;
+function addCart(id) {
+  const cart = JSON.parse(localStorage.getItem("cart")||"[]");
+  cart.push(id);
+  localStorage.setItem("cart", JSON.stringify(cart));
   alert("Adicionado ao carrinho!");
 }
+function goCheckout() { location.href="checkout.html"; }
