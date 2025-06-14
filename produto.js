ce0cd27ab1cbaf0c3b0db3c304d17bdb12e12b1c
MickyMCY <mickybrow22donodoscanais@gmail.com>
2025-06-14 15:13:30 +0200

    Update produto.js

--- produto.js
+++ produto.js
@@ -1,21 +1,17 @@
-const params = new URLSearchParams(location.search);
-const id = parseInt(params.get("id"));
-const prod = allProducts.find(p => p.id === id);
-const div = document.getElementById("product-detail");
-div.innerHTML = `
-  <div class="product-detail">
-    <img src="${prod.img}" alt="${prod.nome}" />
-    <h2>${prod.nome}</h2>
-    <div class="price">${prod.price} MT</div>
-    ${prod.soldOut ? '<div class="sold-out">Esgotado</div>' : ''}
-    <p>Descrição breve do produto vai aqui. Pode personalizar.</p>
-    <button onclick="addCart(${prod.id})" ${prod.soldOut ? 'disabled' : ''}>Adicionar ao carrinho</button>
-    <button onclick="goCheckout()" ${prod.soldOut ? 'disabled' : ''}>Comprar agora</button>
-  </div>`;
-function addCart(id) {
-  const cart = JSON.parse(localStorage.getItem("cart")||"[]");
-  cart.push(id);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert("Adicionado ao carrinho!");
-}
-function goCheckout() { location.href="checkout.html"; }
+const allProducts = [
+  { id:1, nome:"Google Play 2500 MT", img:"https://i.imgur.com/xGg3W2Z.png", price:2500, soldOut:false },
+  { id:2, nome:"iTunes 2500 MT", img:"https://i.imgur.com/fBBA4GI.png", price:2500, soldOut:false },
+  { id:3, nome:"Netflix 1 mês 1000 MT", img:"https://i.imgur.com/RxiGCxD.png", price:1000, soldOut:true },
+  { id:4, nome:"Spotify 3 meses 3000 MT", img:"https://i.imgur.com/6OfvEVi.png", price:3000, soldOut:false },
+  { id:5, nome:"PSN 5000 MT", img:"https://i.imgur.com/mptxBJL.png", price:5000, soldOut:false },
+  { id:6, nome:"Steam 5000 MT", img:"https://i.imgur.com/YzWdhNA.png", price:5000, soldOut:false },
+  { id:7, nome:"Xbox 6000 MT", img:"https://i.imgur.com/ATeY3N4.png", price:6000, soldOut:true },
+  { id:8, nome:"Nintendo 5000 MT", img:"https://i.imgur.com/xyKuGyK.png", price:5000, soldOut:false },
+  { id:9, nome:"Google Play 5000 MT", img:"https://i.imgur.com/xGg3W2Z.png", price:5000, soldOut:false },
+  { id:10, nome:"Netflix 3 meses 2500 MT", img:"https://i.imgur.com/RxiGCxD.png", price:2500, soldOut:false },
+  { id:11, nome:"Steam 10000 MT", img:"https://i.imgur.com/YzWdhNA.png", price:10000, soldOut:false },
+  { id:12, nome:"AirPods 15000 MT", img:"https://i.imgur.com/HZ7YGdw.png", price:15000, soldOut:false },
+  { id:13, nome:"Teclado Gamer 7000 MT", img:"https://i.imgur.com/fFesnpN.png", price:7000, soldOut:false },
+  { id:14, nome:"Mouse RGB 3000 MT", img:"https://i.imgur.com/Lp7dbpa.png", price:3000, soldOut:false },
+  { id:15, nome:"Cartão PSN 12500 MT", img:"https://i.imgur.com/mptxBJL.png", price:12500, soldOut:true }
+];
